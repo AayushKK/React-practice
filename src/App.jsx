9ac7cefa89b070136e28be732e7aafa9bbded1f9
Aayush Kharel <10891kharelaayush@gmail.com>
2025-01-15 10:14:03 +0545

    React basic

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,30 @@
-import React from 'react'
+import Moviecard from './components/Moviecard';
 
 const App = () => {
-  return (
-    <div>
+  const greet = (personName) => {
+    alert(`hello ${personName}`);
+  }
 
-      <h1>hello jee</h1>
-    </div>
+  const msg = (message) => {
+    alert(`${message}`)
+  }
+  return (
+    <>
+      <Moviecard />
+      <div className='p-5'>
+
+
+        <button
+          onClick={() => greet("Shyam")}
+          className='bg-slate-500 text-white px-4 py-2 rounded-md hover:ring-2 ring-purple-500 '>Call Func</button>
+
+        <p onCopy={() => { msg("DO NOT COPY") }}>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempore ipsum at aliquid nobis, eos odit! Qui ex nam quasi aspernatur nihil ea aliquid distinctio itaque sed exercitationem a, aliquam aut.
+        </p>
+
+
+      </div>
+    </>
   )
 }
 
---
+++ src/data.js
@@ -0,0 +1,13 @@
+export const person = {
+  name: 'ram',
+  age: 90
+};
+
+
+export const people = [];
+
+export const greet = () => {
+  return (
+    <div></div>
+  )
+}

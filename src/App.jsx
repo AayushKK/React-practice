c461d274c2f2fb481189d47ef41855f86abe9a32
Aayush Kharel <10891kharelaayush@gmail.com>
2025-01-17 07:49:02 +0545

    updated card

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { Card } from './components/Card';
 import Moviecard from './components/Moviecard';
 
 const App = () => {
@@ -8,9 +9,22 @@ const App = () => {
   const msg = (message) => {
     alert(`${message}`)
   }
+
   return (
     <>
-      <Moviecard />
+      {/* <Moviecard /> */}
+      <div className='flex flex-row'>
+        <Card url={"https://images.unsplash.com/photo-1695121767703-3dfaf0b62d32?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8bm9kZXxlbnwwfHwwfHx8MA%3D%3D"}
+          heading={"Node.js"}
+          subheading={"Emanuele DeilBono"}
+          description={"Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tenetur nobis dolorum repudiandae omnis iure autem culpa "}
+        />
+        <Card url={"https://plus.unsplash.com/premium_photo-1682192408471-a7cf432b3fdc?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8bm9kZXxlbnwwfHwwfHx8MA%3D%3D"}
+          heading={"React.js"}
+          subheading={"Dmitri Nesteruk"}
+          description={"Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tenetur nobis dolorum repudiandae omnis iure autem culpa "}
+        />
+      </div>
       <div className='p-5'>
 
 

065d4be296d7818dc2c1ccc60f153475314f50ec
Aayush Kharel <10891kharelaayush@gmail.com>
2025-01-21 00:40:16 +0545

    footer mobileview completed

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
           <path fill="#00000" fill-opacity="1" d="M0,192L1440,64L1440,320L0,320Z"></path>
         </svg>
-        <div className="absolute inset-x-0 bottom-[20%] text-lg text-white flex flex-col justify-center items-center sm:bottom-1">
+        <div className="absolute inset-x-0 bottom-[15%] text-lg text-white flex flex-col justify-center items-center sm:bottom-0 top-[50%]">
           <h1>Foot notes</h1>
           <h1>This is a footer</h1>
         </div>
